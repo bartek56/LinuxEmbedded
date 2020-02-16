7f6ed63fadd1020cdd9f723fb7209334c2767cbe
Bartosz <bartosz.brzozowski23@gmail.com>
2020-02-16 22:52:06 +0100

    only one session with Youtube

--- configFiles/youtubedl/downloadFromYoutube.py
+++ configFiles/youtubedl/downloadFromYoutube.py
@@ -7,44 +7,56 @@ from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 
 
-PATH='/home/bartosz/Music/Youtube list/'
+PATH='/mnt/TOSHIBA EXT/muzyka/Youtube list/'
+
+def rename_song(songName):
+    songName = songName.replace("(Official Video)", "")
+    songName = songName.replace("( Official Video )", "")
+    songName = songName.replace("(Official Video HD)", "")
+    songName = songName.replace("[Official Video]", "")
+    songName = songName.replace("Official Video", "")
+    songName = songName.replace("(OFFICIAL VIDEO)", "")
+    songName = songName.replace("(Video Official)", "")
+    songName = songName.replace("(Official Video HD)", "")
+    songName = songName.replace("[Video Official]", "")
+    songName = songName.replace("Video Official", "")
+    songName = songName.replace("(VIDEO OFFICIAL)", "")
+    songName = songName.replace("(official video)", "")
+    songName = songName.replace("[Official Music Video]", "")
+  
+    songName = songName.replace("(Oficial Video)", "")
+    songName = songName.replace("(Oficial Video HD)", "")
+    songName = songName.replace("[Oficial Video]", "")
+    songName = songName.replace("Oficial Video", "")
+    songName = songName.replace("(OFICIAL VIDEO)", "")
+    songName = songName.replace("(Video Oficial)", "")
+    songName = songName.replace("(Oficial Video HD)", "")
+    songName = songName.replace("[Video Oficial]", "")
+    songName = songName.replace("Video Oficial", "")
+    songName = songName.replace("(VIDEO OFICIAL)", "")
+
+    songName = songName.replace("(Audio)", "")
+    songName = songName.replace("(Official Audio)", "")
+    songName = songName.replace("[Official Audio]", "")
+
+    songName = songName.replace("(Official Music Video)", "")
+    songName = songName.replace("/", " ")
+    return songName
+
+def convert_song_name(songName):
+    songName = rename_song(songName)
+    songName = songName+".xyz"
+    songName = songName.replace("  .xyz", ".xyz")
+    songName = songName.replace(" .xyz", ".xyz")
+    songName = songName.replace(".xyz", "")
+    return songName
 
 def rename_song_file(path, fileName):
 
     originalFileName = fileName 
-    fileName = fileName.replace("(Official Video)", "")
-    fileName = fileName.replace("( Official Video )", "")
-    fileName = fileName.replace("(Official Video HD)", "")
-    fileName = fileName.replace("[Official Video]", "")
-    fileName = fileName.replace("[Official Video]", "")   
-    fileName = fileName.replace("[OFFICIAL VIDEO]", "")
-    fileName = fileName.replace("(OFFICIAL VIDEO)", "")
-    fileName = fileName.replace("(Video Official)", "")
-    fileName = fileName.replace("(Official Video HD)", "")
-    fileName = fileName.replace("[Video Official]", "")
-    fileName = fileName.replace("Video Official", "")
-    fileName = fileName.replace("(VIDEO OFFICIAL)", "")
-    fileName = fileName.replace("(official video)", "")    
-    fileName = fileName.replace("[Official Music Video]", "")
+    
+    fileName = rename_song(fileName)
 
-    fileName = fileName.replace("(Oficial Video)", "")
-    fileName = fileName.replace("(Oficial Video HD)", "")
-    fileName = fileName.replace("[Oficial Video]", "")
-    fileName = fileName.replace("Oficial Video", "")
-    fileName = fileName.replace("(OFICIAL VIDEO)", "")
-    fileName = fileName.replace("(Video Oficial)", "")
-    fileName = fileName.replace("(Oficial Video HD)", "")
-    fileName = fileName.replace("[Video Oficial]", "")
-    fileName = fileName.replace("Video Oficial", "")
-    fileName = fileName.replace("(VIDEO OFICIAL)", "")
-
-    fileName = fileName.replace("(Audio)", "")
-    fileName = fileName.replace("(Official Audio)", "")
-    fileName = fileName.replace("[Official Audio]", "")
-
-    fileName = fileName.replace("(Official Music Video)", "")
-    fileName = fileName.replace("/", " ")
-     
     fileName = fileName.replace("  .mp3", ".mp3")
     fileName = fileName.replace(" .mp3", ".mp3")
 
@@ -54,57 +66,80 @@ def rename_song_file(path, fileName):
 
     return fileName
 
-def add_metadata(playlistName, fileName):
-    songName=fileName.replace(".mp3","")
-    path=PATH+playlistName
-    fileNameWithPath = os.path.join(path, fileName)
-    audio = MP3(fileNameWithPath, ID3=EasyID3)
-    result = audio.pprint()
-    albumName="YT "+playlistName
-    if(albumName not in result):
-      trackNumber = len([f for f in os.listdir(path) if f.endswith('.mp3')])
-      trackNumber += 1
-      metatag = EasyID3(fileNameWithPath)
+def convert_songname_on_metadata(songName):
+    slots = songName.split(" - ")
+    metadata ={ 'tracknumber': "1",}
+    if len(slots) == 2:
+      metadata['artist'] = slots[0]
+      metadata['title'] = slots[1]
+    elif len(slots) < 2:
+      slots = songName.split("-")
+      if len(slots) == 2:
+        metadata['artist'] = slots[0]
+        metadata['title'] = slots[1]
+      else:
+        metadata['title'] = songName
+        metadata['artist'] = ""
+    else:
+      metadata['artist'] = slots[0]
+      name=""
+      i=0
+      for slots2 in slots:
+        if i > 0:
+          if i > 1:
+            name+="-"
+          name+=slots[i]
+        i=i+1  
+      metadata['title'] = name
+
+    return metadata
+
+def update_metadata(playlistName):
+      path=PATH+playlistName
+      albumName="YT "+playlistName
+
+      filesList = [f for f in os.listdir(path) if f.endswith('.mp3')]
+      for x in range(len(filesList)):
+        originalFileName = filesList[x]
+
+        newFileName = rename_song_file(path, originalFileName)
+        newSongName = newFileName.replace(".mp3", "")
+        
+        metadataSongName = convert_songname_on_metadata(newSongName)
+        newFileNameWithPath = os.path.join(path, newFileName)
+
+        metatag = EasyID3(newFileNameWithPath)
+        metatag['album'] = albumName
+        metatag['artist'] = metadataSongName['artist']
+        metatag['title'] = metadataSongName['title']
+        metatag.save()
+        print "[ID3] Updated metadata"
+
+def add_metadata(trackNumber, playlistName, songName):
+      path=PATH+playlistName
+      fileName=songName+".mp3"
+#      fileNameWithPath = os.path.join(path, fileName)
+#      metadataID = EasyID3(path+'/'+fileName)
+      metadataSongName = convert_songname_on_metadata(songName)
+
+      audio = MP3(PATH+playlistName+'/'+fileName, ID3=EasyID3)
+      result = audio.pprint()
+      albumName="YT "+playlistName
+#    if(albumName not in result):
+      metatag = EasyID3(PATH+playlistName+'/'+fileName)
       metatag['album'] = albumName
       metatag['tracknumber'] = str(trackNumber)
-      slots = songName.split(" - ")
-      info = " "
-      if len(slots) == 2:
-        metatag['artist'] = slots[0]
-        metatag['title'] = slots[1]
-        info = " " + slots[0] + " " + slots[1]
-      else:
-        slots = songName.split("-")
-        if len(slots) == 2:
-          metatag['artist'] = slots[0]
-          metatag['title'] = slots[1]
-          info = " " + slots[0] + " " + slots[1]
-        else:  
-          metatag['title'] = songName
-
+      metatag['artist'] = metadataSongName['artist']
+      metatag['title'] = metadataSongName['title']
       metatag.save()
-      print ("[ID3] Added metadata")
-
-def download_video_as_mp3(url, playlistName, songName):
-    path=PATH+playlistName
-    ydl_opts = {
-            'format': 'bestaudio/best',
-            'download_archive': path+'/downloaded_songs.txt',
-            'addmetadata': True,
-            'outtmpl': path+'/'+songName+'.%(ext)s',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-             }],
-            'ignoreerrors': True
-               } 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      print "[ID3] Added metadata"
         
-        ydl.download([url])
-
 def download_video_playlist(url, playlistName):
   path=PATH+playlistName
+  if not os.path.exists(path):
+    os.makedirs(path)
+  trackNumber = len([f for f in os.listdir(path) if f.endswith('.mp3')])
+
   ydl_opts = {
           'ignoreerrors': True, 
           'format': 'bestaudio/best',
@@ -115,29 +150,32 @@ def download_video_playlist(url, playlistName):
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
-             }],
-          'playlist-start': 8
+             }]
+#          'playlist-start': 8
           }  
-  #results = youtube_dl.YoutubeDL(ydl_opts).extract_info(url, download=False)
+  results = youtube_dl.YoutubeDL(ydl_opts).extract_info(url)
   
-  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-  
-  filesList = [f for f in os.listdir(path) if f.endswith('.mp3')]
-  
-  for x in range(len(filesList)):
-     originalFileName = filesList[x]
-     newFileName = rename_song_file(path, originalFileName)
-     add_metadata(playlistName, newFileName)
+#  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#        results = ydl.extract_info(url)
+
+  songsTitleList = [i['title'] for i in results['entries']]
+  for x in range(len(songsTitleList)):
+     songName = songsTitleList[x]
+     songName = convert_song_name(songName)
+     trackNumber+=1
+     add_metadata(trackNumber, playlistName, songName)
      
+
+
 def main():
    now = datetime.now()
    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
    print("---------  " + dt_string + "  ---------") 
 
+   #update_metadata("muzyka filmowa")
 
 #  PART 1
-   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfiCtXJW-I0OASdxMc7sGHn5", "chillout")
+#   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfiCtXJW-I0OASdxMc7sGHn5", "chillout")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfgHTfI_P_BaACTGN2Km_4Yk", "Bachata")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfjahtnJWMf2cW6TDmpfTUqk", "spokojne-sad")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfjKkH06p81TLmGItIfoMnb5", "relaks")
@@ -147,9 +185,9 @@ def main():
 
 
 #  PART 2
-   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfgDX6QR1isPJEUNkWRbPa0e", "polskie hity")
+#   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfgDX6QR1isPJEUNkWRbPa0e", "polskie hity")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfgMkKYQ8zK1wPzLE7nuYbYk", "stare ale jare")
-#   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfg4ur-Bk9PCdqguhKoHCfMD", "muzyka filmowa")
+   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfg4ur-Bk9PCdqguhKoHCfMD", "muzyka filmowa")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfgcWvb2c97oOX773DQHnhjQ", "wesele stare hity")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfh09hGVx2_RreHvFmenLMma", "wesele pop")
 #   download_video_playlist("https://www.youtube.com/playlist?list=PL6uhlddQJkfjJaHNCH5XacdQdGOOCjuej", "wesele disco-polo")

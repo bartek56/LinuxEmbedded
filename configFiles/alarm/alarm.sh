aa04dc7b3541dffb7e87292109c98aba6d64416f
bartek_Buildroot <bartosz.brzozowski23@gmail.com>
2020-05-06 12:31:18 +0200

    theNewestSong on alarm

--- configFiles/alarm/alarm.sh
+++ configFiles/alarm/alarm.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-minVolume=30
-maxVolume=90
-defaultVolume=40
-growingVolume=10
-growingSpeed=20
-playlist="alarm"
-theNewestSongs=false
+minVolume=25
+maxVolume=80
+defaultVolume=35
+growingVolume=8
+growingSpeed=25
+playlist="Youtube"
+theNewestSongs=true
 
 
 set -e
@@ -17,25 +17,32 @@ if [ "$theNewestSongs" == true ]; then
     countSongs=0
     lastDays=0
 
-    while [ $countSongs -le 10 ]; do
- 	lastDays=$((lastDays + 1))
-        echo $lastDays
-	countSongs=$(find "/mnt/TOSHIBA EXT/muzyka/Youtube list" -type f -mtime -$lastDays -name "*.mp3" | wc -l)
-        echo $countSongs
+    while [ $countSongs -le 2 ]; do
+ 	      lastDays=$((lastDays + 1))
+	      countSongs=$(find "/mnt/TOSHIBA EXT/muzyka/Youtube list" -type f -mtime -$lastDays -name "*.mp3" | wc -l)
     done
 
     musicList=$(find "/mnt/TOSHIBA EXT/muzyka/Youtube list" -type f -mtime -$lastDays -name "*.mp3" -exec basename '{}' ';' | head -n 10 )
-   
+    
     mpc --wait clear
+		songs=()
     for songName in $musicList; do
-        mpc --wait listall | grep $songName | mpc add
+				songs+=($songName)
+#        mpc --wait listall | grep $songName | mpc add
     done
+
+
+    # revert list   
+    for ((i=${#songs[@]}-1; i>=0; i-- )); do
+        mpc --wait listall | grep ${songs[$i]} | mpc add
+    done
+		mpc random off
 else 
     mpc clear
     mpc --wait load $playlist
+    mpc random on
 fi
 
-mpc random on
 mpc volume $minVolume
 mpc play
 

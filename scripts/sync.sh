eb06427dd6ec840ac3e46bee3a18ab2f53eb3c9b
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2022-05-24 20:49:13 +0200

    secure sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,62 +3,63 @@
 # synchronization between laptop and Brzozowscy server
 # catalogs:
 # Dell external drive:
-#  dokumenty            remote   Brzozowscy server (priv)    remoteSyncFromDell
-#  images_system        local    Brzozowscy server (priv)    localSyncInBrzozowscy
-#  instalki programy    local    Brzozowscy server (share)   localSyncInBrzozowscy
-#  Kamera sportowa      local    Brzozowscy server (priv)    localSyncInBrzozowscy
-#  szkola               local    Brzozowscy server (priv)    localSyncInBrzozowscy
-#  taniec               remote   Brzozowscy server (share)   remoteSyncFromDell
-#  tapety               remote   Brzozowscy server (priv)    remoteSyncFromDell
-#  VHS na DVD                    Brzozowscy server (priv)    
-#  Zdjecia              remote   Brzozowscy server (priv)    remoteSyncFromDell
-#  
+#  dokumenty            Brzozowscy server (priv)    syncFromDell localSyncInBrzozowscy
+#  Zdjecia              Brzozowscy server (priv)    syncFromDell localSyncInBrzozowscy
+#  tapety               Brzozowscy server (share)   syncFromDell localSyncInBrzozowscy
+#  images_system        Brzozowscy server (priv)    localSyncInBrzozowscy
+#  szkola               Brzozowscy server (priv)    localSyncInBrzozowscy
+#  taniec               Brzozowscy server (share)   localSyncInBrzozowscy
+#  instalki programy    Brzozowscy server (share)   localSyncInBrzozowscy
+#  VHS na DVD           Brzozowscy server (priv)    localSyncInBrzozowscy 
+# TODO
+#  Kamera sportowa      Brzozowscy server (priv)    localSyncInBrzozowscy 
+
+
 # MediaServer
-#  movies                        Brzozowscy server (share)   
-#  TVShows                       Brzozowscy server (share)
-#  music                remote   Brzozowscy server (share)   remoteSyncFromBrzozowscy
-#  books                remote   Brzozowscy server (share)   remoteSyncFromBrzozowscy
-#  audiobooks           remote   Brzozowscy server (share)   remoteSyncFromBrzozowscy
+#  movies               Brzozowscy server (share)   WITHOUT_BACKUP
+#  TVShows              Brzozowscy server (share)   WITHOUT_BACKUP
+#  music                Brzozowscy server (share)   syncFromBrzozowscy
+#  books                Brzozowscy server (share)   syncFromBrzozowscy
+#  audiobooks           Brzozowscy server (share)   syncFromBrzozowscy
 
 
 # VARIABLES: user@address
 BRZOZOWSCY_REMOTE=
 BRZOZOWSCY_REMOTE_ROOT=
 MEDIASERVER_REMOTE=
+BRZOZOWSCY_PORT=
+MEDIASERVER_PORT=
 
-remoteSyncFromDell()
+syncFromDell()
 {
     printf "Remote sync From Dell\n"
     printf "mount \n"
-    ssh -p 20022 -t $BRZOZOWSCY_REMOTE_ROOT 'mount /dev/sda1 /media/priv'
+    ssh -p $BRZOZOWSCY_PORT -t $BRZOZOWSCY_REMOTE_ROOT 'mount /dev/sda1 /media/priv'
 
     printf "\nDOKUMENTY \n"
-    rsync -srvazz -e 'ssh -p 20022' --delete "/home/bartosz/Documents/dokumenty" "$BRZOZOWSCY_REMOTE:/media/priv/"
-
-    printf "\nTANIEC \n"
-    rsync -srvazz -e 'ssh -p 20022' --delete "/home/bartosz/Videos/taniec" "$BRZOZOWSCY_REMOTE:/media/shareTV/share/"
+    rsync -srvazz -e 'ssh -p $BRZOZOWSCY_PORT' --delete "/home/bartosz/Documents/dokumenty" "$BRZOZOWSCY_REMOTE:/media/priv/"
 
     printf "\nTAPETY \n"
-    rsync -srvazz -e 'ssh -p 20022' --delete "/home/bartosz/Pictures/tapety" "$BRZOZOWSCY_REMOTE:/media/priv/"
+    rsync -srvazz -e 'ssh -p $BRZOZOWSCY_PORT' --delete "/home/bartosz/Pictures/tapety" "$BRZOZOWSCY_REMOTE:/media/shareTV/share/"
 
     printf "\nZDJECIA \n"
-    rsync -srvazz -e 'ssh -p 20022' --delete "/home/bartosz/Pictures/z telefonu/7 Xiaomi Mi" "$BRZOZOWSCY_REMOTE:/media/priv/Zdjęcia/z telefonu/"
+    rsync -srvazz -e 'ssh -p $BRZOZOWSCY_PORT' --delete "/home/bartosz/Pictures/z telefonu/7 Xiaomi Mi" "$BRZOZOWSCY_REMOTE:/media/priv/Zdjęcia/z telefonu/"
 
     printf "\numount \n"
-    ssh -p 20022 -t $BRZOZOWSCY_REMOTE_ROOT 'umount /media/priv'
+    ssh -p $BRZOZOWSCY_PORT -t $BRZOZOWSCY_REMOTE_ROOT 'umount /media/priv'
 }
 
-remoteSyncFromBrzozowscy()
+syncFromBrzozowscy()
 {
     printf "Remote Sync from Brzozowscy\n"
     printf "MUZYKA \n"
-    rsync -srvazz --delete "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/muzyka/" "/media/shareTV/share/music/"
+    rsync -srvazz -e 'ssh -p $MEDIASERVER_PORT' --delete "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/muzyka/" "/media/shareTV/share/music/"
 
     printf "\nKSIAZKI \n"
-    rsync -srvazz --delete "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/books" "/media/shareTV/share/"
+    rsync -srvazz -e 'ssh -p $MEDIASERVER_PORT'--delete "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/books" "/media/shareTV/share/"
 
     printf "\nAUDIOBOOKS \n"
-    rsync -srvazz --delete "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/Audiobooks" "/media/shareTV/share/"
+    rsync -srvazz -e 'ssh -p $MEDIASERVER_PORT'--delete "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/Audiobooks" "/media/shareTV/share/"
 }
 
 
@@ -84,21 +85,10 @@ localSyncInBrzozowscy()
     rsync -srvazz --delete "/media/programmer/Dell HDD/taniec" "/media/shareTV/share/"
 
     printf "\nTAPETY \n"
-    rsync -srvazz --delete "/media/programmer/Dell HDD/tapety" "/media/programmer/priv/"
+    rsync -srvazz --delete "/media/programmer/Dell HDD/tapety" "/media/shareTV/share/"
 
     printf "\nZDJECIA \n"
     rsync -srvazz --delete "/media/programmer/Dell HDD/Zdjęcia" "/media/programmer/priv/"
-
-}
-
-tvshowsToHardDrive()
-{
-    printf "\nGALILEO \n"
-    filesList=$(rsync -snrvazz --ignore-existing "/media/shareTV/share/TVShows/Galileo/" "$MEDIASERVER_REMOTE:/mnt/TOSHIBA EXT/Wideo/TV Shows/Galileo/" | grep -E "*.mkv|*.ts|*.txt")
-    for file in $filesList; do
-        printf "%s\n" $file
-        cp -n "/media/shareTV/share/TVShows/Galileo/$file" "/media/programmer/Dell HDD/Galileo/"
-    done
 }
 
 set -e
@@ -109,16 +99,12 @@ if [[ "$argument" == "mediaserver" ]]; then
     printf "mediaserver\n"
 elif [[ "$argument" == "brzozowscy" ]]; then
     printf "brzozowscy\n"
-    remoteSyncFromBrzozowscy
+    syncFromBrzozowscy
 elif [[ "$argument" == "dell" ]]; then 
     printf "dell\n"
-    remoteSyncFromDell
+    syncFromDell
 elif [[ "$argument" == "local" ]]; then 
     printf "local\n"
     localSyncInBrzozowscy
-elif [[ "$argument" == "galileo" ]]; then 
-    printf "Galileo to HardDrive\n"
-    tvshowsToHardDrive
 fi
-
 
